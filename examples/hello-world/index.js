aabd5f67778fc4b91a7362dce3a813bf3e1bdd1a
kamilmarchewka <93246438+kamilmarchewka@users.noreply.github.com>
2026-05-12 18:29:45 +0200

    Update index.js

--- examples/hello-world/index.js
+++ examples/hello-world/index.js
@@ -10,6 +10,6 @@ app.get('/', function(req, res){
 
 /* istanbul ignore next */
 if (!module.parent) {
-  app.listen(3000, '0.0.0.0');
+  app.listen(3000);
   console.log('Express started on port 3000');
 }
